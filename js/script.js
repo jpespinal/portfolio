f91e70a7e446e9b914f25cca0946427567f5a488
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-03-01 20:34:03 -0500

    Attempting to solve error in code.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var nav = $("#top nav ul").children();
-nav[0].onload(addClass("active"));
-nav.click(function() {this.addClass("active")});
-var sections = $("#main").children();
-sections.addClass("hide-section");
-sections.click(sections.toggleClass("hide-section"));
+nav[0].load(function() {addClass("active")});
+
+// nav.click(function() {this.addClass("active")});
+
+// var sections = $("#main").children();
+// sections.addClass("hide-section");
+// sections.click(sections.toggleClass("hide-section"));
