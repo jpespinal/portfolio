c69737c5a7f628d753d651a069ed62bbc508b67d
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-03-01 20:30:50 -0500

    Attempting to load page with About section nav link with active class.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var nav = $("#top nav ul").children();
+nav[0].onload(addClass("active"));
 nav.click(function() {this.addClass("active")});
 var sections = $("#main").children();
 sections.addClass("hide-section");
-sections.click(sections.toggleClass(removeClass("hide-section")));
+sections.click(sections.toggleClass("hide-section"));
