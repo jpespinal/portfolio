5c4176a0028519a1fe338d6ab4ed01f01cd93371
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-03-01 20:12:38 -0500

    Adding active class to Nav via jQuery.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
+var nav = $("#top nav ul").children();
+nav.click(function() {this.addClass("active")});
 var sections = $("#main").children();
 sections.addClass("hide-section");
+sections.click(sections.toggleClass(removeClass("hide-section")));
