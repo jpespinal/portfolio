afa544484254c1b033764d117673cc3d458f0eba
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-03-02 11:17:20 -0500

    Wrote code to switch active class among nav links.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Create Variables pointing to all Nav links and the About section link.
-var nav = $("#top nav ul").children();
-var aboutLink = nav[0];
+var siteNav = $("#top nav ul").children();
+var aboutLink = siteNav[0];
 
 // Create Variables poingting to all Sections and the About section.
-var sections = $("#main").children();
-var aboutSection = sections[0];
+var siteSections = $("#main").children();
+var aboutSection = siteSections[0];
 
 // Hide all major sections by adding CSS class.
 sections.addClass("hide-section");
@@ -19,3 +19,12 @@ $(document).ready(function() {
   $(aboutLink).addClass("active");
   $(aboutSection).removeClass("hide-section");
 });
+
+// 1. Capture click on Nav links.
+$("siteNav").click(function() {
+  var nonActive = $("siteNav").hasClass("active");
+  nonActive.removeClass("active");
+  $(this).addClass("active");
+});
+  // 1.1 Add active class to clicked link. Reveal linked Section.
+  // 1.2 Remove active class from previously clicked link. Hide all other Sections.
