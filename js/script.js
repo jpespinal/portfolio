7f7c5a58542036d25ad352d2e95c12f8ae957f7c
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-03-03 10:29:55 -0500

    Restored working jQuery code.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
 // Create Variables pointing to all Nav links and the About section link.
 var siteNav = $("#top nav ul li").children();
-console.log(siteNav);
+var aboutLink = siteNav[0];
 
-// var aboutLink = siteNav[0];
-
-// // Create Variables poingting to all Sections and the About section.
-// var siteSections = $("#main").children();
-// var aboutSection = siteSections[0];
+// Create Variables poingting to all Sections and the About section.
+var siteSections = $("#main").children();
+var aboutSection = siteSections[0];
 
 // // Hide all major sections by adding CSS class.
-// siteSections.addClass("hide-section");
+siteSections.addClass("hide-section");
 
-// // Upon document being ready, make user "arrive" at the About section by removing the hide class on it.
-// // Add active class to About section link.
-// $(function() {
-//   $(aboutLink).addClass("active");
-//   $(aboutSection).removeClass("hide-section");
-// });
+// Upon document being ready, make user "arrive" at the About section by removing the hide class on it.
+// Add active class to About section link.
+$(function() {
+  $(aboutLink).addClass("active");
+  $(aboutSection).removeClass("hide-section");
+});
 
 // 1. Capture click on Nav anchor.
   // 1.1 Add active class to clicked link. Reveal linked Section.
