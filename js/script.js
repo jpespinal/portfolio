2e78567f6c4afe408971f6381d99b84ba0add459
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-03-07 14:23:48 -0500

    Changed error in code.

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ $("#top a").click(function() {
   // 1.4 Hide all other sections.
   siteSections.addClass("hide-section");
   // 1.5 Reveal proper section.
-  $(("main").children()).each(function() {
+  $(("#main").children()).each(function() {
     var hrefSection = $(this).attr("href");
     if(hrefLink === hrefSection) {
       removeClass("hide-section");
