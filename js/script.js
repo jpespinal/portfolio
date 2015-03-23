3d725e44d67b737c526fca282d926166453c9bf2
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-03-23 10:00:31 -0400

    Hiding Site Sections via jQuery. Added About section graphic.

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ var siteSections = $("#main").children();
 var aboutSection = siteSections[0];
 
 // // Hide all major sections by adding CSS class.
-siteSections.addClass("hide-section");
+siteSections.hide();
 
 // Upon document being ready, make user "arrive" at the About section by removing the hide class on it.
 // Add active class to About section link.
 $(function() {
   $(aboutLink).addClass("active");
-  $(aboutSection).removeClass("hide-section");
+  $(aboutSection).show();
 });
 
 // 1. Capture click on Nav anchor.
@@ -24,10 +24,7 @@ $("#top a").click(function() {
   $(this).addClass("active");
   // 1.3 Identify proper section.
   var hrefLink = $(this).attr("href");
-  hrefLink = hrefLink.slice(0);
   // 1.4 Hide all other sections.
-  siteSections.addClass("hide-section");
+  siteSections.hide();
   // 1.5 Reveal proper section.
-  var revealSection = siteSections.hasId(hrefLink);
-  revealSection.removeClass("hide-section");
 });
