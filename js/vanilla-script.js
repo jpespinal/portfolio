7159b66fc0f8807dfbbe023e3212f852a73d0f97
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-07-12 19:28:25 -0400

    Using setAttribute method to set HTML class.

--- js/vanilla-script.js
+++ js/vanilla-script.js
@@ -18,47 +18,46 @@ console.log(aboutSection);
 
  // Hide all major sections by adding CSS class.
 for (var i = 0; i < siteSections.length; i++) {
-    siteSections[i].classList.add("hide");
+    siteSections[i].setAttribute("class", "hide");
+}
 
-};
-
-// Upon document being ready, make user "arrive" at the About
-// section by removing the hide class on it.
-
-// Add active class to About section link.
-window.onload = function() {
-    aboutLink.classList.add("active");
-    aboutSection.classList.remove("hide");
-
-    // Give the site "multi-page" functionality
-    // by hiding and revealing the appropriate sections.
-    for (i = 0; i < siteNav.length; i++) {
-
-        // Capture click on Nav anchor.
-        siteNav[i].addEventListener("click", function() {
-
-            console.log(this);
-
-            // Identify proper section.
-            var hrefLink = this.getAttribute("href");
-            hrefLink = hrefLink.slice(1, hrefLink.length);
-            console.log(hrefLink);
-
-            // Remove active class from all links.
-            for (var j = 0; j < siteNav.length; j++) {
-                siteNav[j].classList.remove("active");
-            }
-
-            // Add active class to clicked link.
-            this.classList.add("active");
-
-            // Hide all other sections.
-            for (var k = 0; k < siteSections.length; k++) {
-                siteSections[k].classList.add("hide");
-            }
-
-            // Find proper section, reveal it.
-            document.getElementById(hrefLink).classList.remove("hide");
-        });
-    }
-};
+//// Upon document being ready, make user "arrive" at the About
+//// section by removing the hide class on it.
+//
+//// Add active class to About section link.
+//window.onload = function() {
+//    aboutLink.setAttribute("class", "active");
+//    aboutSection.removeAttribute("class", "hide");
+//
+//    // Give the site "multi-page" functionality
+//    // by hiding and revealing the appropriate sections.
+//    for (i = 0; i < siteNav.length; i++) {
+//
+//        // Capture click on Nav anchor.
+//        siteNav[i].addEventListener("click", function() {
+//
+//            console.log(this);
+//
+//            // Identify proper section.
+//            var hrefLink = this.getAttribute("href");
+//            hrefLink = hrefLink.slice(1, hrefLink.length);
+//            console.log(hrefLink);
+//
+//            // Remove active class from all links.
+//            for (var j = 0; j < siteNav.length; j++) {
+//                siteNav[j].classList.remove("active");
+//            }
+//
+//            // Add active class to clicked link.
+//            this.classList.add("active");
+//
+//            // Hide all other sections.
+//            for (var k = 0; k < siteSections.length; k++) {
+//                siteSections[k].classList.add("hide");
+//            }
+//
+//            // Find proper section, reveal it.
+//            document.getElementById(hrefLink).classList.remove("hide");
+//        });
+//    }
+//};
