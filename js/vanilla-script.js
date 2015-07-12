c2649066294dc0e1f78397562ec2ba2e06d6311e
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-07-12 19:59:29 -0400

    Updated rest of code with the new variable names.

--- js/vanilla-script.js
+++ js/vanilla-script.js
@@ -30,34 +30,34 @@ window.onload = function() {
     aboutPage.classList.remove("hide");
 };
 
-    // Give the site "multi-page" functionality
-    // by hiding and revealing the appropriate sections.
-    for (i = 0; i < siteNav.length; i++) {
+// Give the site "multi-page" functionality
+// by hiding and revealing the appropriate sections.
+for (i = 0; i < siteNav.length; i++) {
 
-        // Capture click on Nav anchor.
-        siteNav[i].addEventListener("click", function() {
+    // Capture click on Nav anchor.
+    siteNav[i].addEventListener("click", function() {
 
-            console.log(this);
+        console.log(this);
 
-            // Identify proper section.
-            var hrefLink = this.getAttribute("href");
-            hrefLink = hrefLink.slice(1, hrefLink.length);
-            console.log(hrefLink);
+        // Identify proper section.
+        var hrefLink = this.getAttribute("href");
+        hrefLink = hrefLink.slice(1, hrefLink.length);
+        console.log(hrefLink);
 
-            // Remove active class from all links.
-            for (var j = 0; j < siteNav.length; j++) {
-                siteNav[j].classList.remove("active");
-            }
+        // Remove active class from all links.
+        for (var j = 0; j < siteNav.length; j++) {
+            siteNav[j].classList.remove("active");
+        }
 
-            // Add active class to clicked link.
-            this.classList.add("active");
+        // Add active class to clicked link.
+        this.classList.add("active");
 
-            // Hide all other sections.
-            for (var k = 0; k < siteSections.length; k++) {
-                siteSections[k].classList.add("hide");
-            }
+        // Hide all other sections.
+        for (var k = 0; k < sitePages.length; k++) {
+            sitePages[k].classList.add("hide");
+        }
 
-            // Find proper section, reveal it.
-            document.getElementById(hrefLink).classList.remove("hide");
-        });
-    }
+        // Find proper section, reveal it.
+        document.getElementById(hrefLink).classList.remove("hide");
+    });
+}
