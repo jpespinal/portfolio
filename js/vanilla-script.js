b0bf271b7208d7dff59cb039e14e5b48b941ed44
Juan Pablo Espinal <jpespinal@jpespinal.com>
2015-07-20 18:21:10 -0400

    Changed variable names in script. Identified elements that will have their colors changed, commented that code out of the script.

--- js/vanilla-script.js
+++ js/vanilla-script.js
@@ -1,20 +1,45 @@
-// Create Variables pointing to all Nav links and the About
-// section link.
-var siteNav = document.getElementsByClassName("site__header__navlink");
+// Store Site Nav in variable.
+var siteNav = document.getElementsByClassName("site__header")[0];
+
+// Store all Nav links and the About section link in variables.
+var siteLinks = document.getElementsByClassName("site__header__navlink");
 var aboutLink = siteNav[0];
 
-// Create Variables pointing to all Sections and the About
-// section.
+// Store all Site "Pages" and the About section in variables.
 var sitePages = document.getElementsByClassName("page");
 var aboutPage = sitePages[0];
 
- // Hide all major sections by adding CSS class.
-for (var i = 0; i < sitePages.length; i++) {
-    sitePages[i].classList.add("hide");
-}
+// Create Object literal that will store Site colors.
+var siteColors = {
+    aboutPage: {
+        backgroundColorOne: "#DF4949",
+        backgroundColorTwo: "#F35D5D",
+        headingColor: "#EFC94C"
+    },
+    skillsPage: {
+        backgroundColorOne: "#45B29D",
+        backgroundColorTwo: "#59C6B1",
+        headingColor: "#334D5C"
+    },
+    projectsPage: {
+        backgroundColorOne: "#EFC94C",
+        backgroundColorTwo: "#FFDD60",
+        headingColor: "#DF4949"
+    },
+    contactPage: {
+        backgroundColorOne: "#334D5C",
+        backgroundColorTwo: "#476170",
+        headingColor: "#45B29D"
+    }
+};
 
 // Upon document being ready, make user "arrive" at the About
 // section by removing the hide class on it.
+
+// Hide all major sections by adding CSS class.
+for (var i = 0; i < sitePages.length; i++) {
+    sitePages[i].classList.add("hide");
+}
 
 // Add active class to About section link.
 window.onload = function() {
@@ -24,21 +49,21 @@ window.onload = function() {
 
 // Give the site "multi-page" functionality
 // by hiding and revealing the appropriate sections.
-for (i = 0; i < siteNav.length; i++) {
+for (i = 0; i < siteLinks.length; i++) {
 
     // Capture click on Nav anchor.
-    siteNav[i].addEventListener("click", function() {
-
-        console.log(this);
+    siteLinks[i].addEventListener("click", function() {
 
         // Identify proper section.
+        // Get the href of the link that was clicked on.
         var hrefLink = this.getAttribute("href");
+
+        // Remove the pound symbol.
         hrefLink = hrefLink.slice(1, hrefLink.length);
-        console.log(hrefLink);
 
         // Remove active class from all links.
-        for (var j = 0; j < siteNav.length; j++) {
-            siteNav[j].classList.remove("active");
+        for (var j = 0; j < siteLinks.length; j++) {
+            siteLinks[j].classList.remove("active");
         }
 
         // Add active class to clicked link.
@@ -49,7 +74,14 @@ for (i = 0; i < siteNav.length; i++) {
             sitePages[k].classList.add("hide");
         }
 
-        // Find proper section, reveal it.
+        // Find proper section by using href, and reveal section by removing the css hide class.
         document.getElementById(hrefLink).classList.remove("hide");
     });
 }
+
+/*
+siteNav.style.background("backgroundColorTwo");
+document.getElementById(hrefLink).style.background("backgroundColorOne");
+hrefLink.getElementsByClassName("title").style.color("headingColor");
+hrefLink.getElementsByClassName("subtitle").style.color("headingColor");
+*/
